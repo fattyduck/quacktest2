b31c24d970a76d34e98ebfe8247a2ab9c2a886fc
fattyduck <dison.ruan@yahoo.com>
2015-04-26 09:50:38 -0400

    Working on Derek Banas Tutorials

---
+++ src/nyc/c4q/fattyduck/Car.java
@@ -0,0 +1,7 @@
+package nyc.c4q.fattyduck;
+/**
+ * Created by fattyduck on 4/26/15.
+ */
+public class Car
+{
+}
--- src/nyc/c4q/fattyduck/Vehicles.java
+++ src/nyc/c4q/fattyduck/Vehicles.java
@@ -4,4 +4,28 @@ package nyc.c4q.fattyduck;
  */
 public class Vehicles
 {
+    double speed = 0;
+    int wheels = 2;
+
+    double PI = 3.14;
+
+    int getWheel(){
+        return wheels;
+    }
+
+    void setWheel(int numOfWheel){
+        this.wheels=numOfWheel;
+    }
+
+    double getSpeed(){
+        return speed;
+    }
+
+    void setSpeed(double speed){
+        this.speed = speed;
+    }
+
+
+
+
 }
