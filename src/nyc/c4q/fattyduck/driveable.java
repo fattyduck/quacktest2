e2ff4506cd2d44f6c8f6d6a6cd1302262a6446fc
fattyduck <dison.ruan@yahoo.com>
2015-04-26 09:47:29 -0400

    Working on Derek Banas Tutorials

---
+++ src/nyc/c4q/fattyduck/driveable.java
@@ -0,0 +1,25 @@
+package nyc.c4q.fattyduck;
+/**
+ * Created by fattyduck on 4/26/15.
+ */
+public interface driveable
+{
+    //objects of the interface are public abstract by default
+    //objects in a interface should end with ;
+    //variables of the interface are final by default
+
+    double PI = 3.14;
+
+    int getWheel();
+
+    void setWheel(int numOfWheel);
+
+    double getSpeed();
+
+    void setSpeed(double speed);
+
+
+
+
+
+}
